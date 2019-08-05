921c18fd5b8b474ed5a8156cf9263ea788e7efd7
dome4 <dominic.seitz@gmx.de>
2019-08-05 21:05:06 +0200

    api path updated

--- MemberTool/ClientApp/src/environments/environment.prod.ts
+++ MemberTool/ClientApp/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
 
   // URL of development API
-  peopleApiUrl: 'https://member-tool.azurewebsites.net/api/people'
+  peopleApiUrl: '/api/people'
 };
